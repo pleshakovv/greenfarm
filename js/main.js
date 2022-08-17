0abc1a2f1fc22d7087bf32692d7f79bcd687c0de
pleshakovv <102418049+pleshakovv@users.noreply.github.com>
2022-08-17 12:32:41 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.addEventListener('scroll', (e) =>{
+   const topline = document.querySelectorAll('.line--top'),
+         bottomline = document.querySelectorAll('.line--bottom'),
+         scrolled = window.pageYOffset,
+         rate = scrolled * 0.5,
+         heroImage = document.querySelector('.hero__photo');
+
+   topline.forEach(function(line) {
+      line.style.transform = 'translate3d(0px,'+rate+'px, 0)';
+   });
+
+   bottomline.forEach(function(line) {
+      line.style.transform = 'translate3d(0px,-'+rate+'px, 0)';
+   });
+
+   heroImage.style.transform = 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, '+rate+', 0, 1)';
+});
+
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   slidesPerView: 3,
+   spaceBetween: 20,
+   centeredSlides: true,
+   grabCursor: true,
+
+ 
+   // And if we need scrollbar
+   scrollbar: {
+     el: '.swiper-scrollbar',
+     clickable: true,
+   },
+ });
